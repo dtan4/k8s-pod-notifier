b8625be00461d0bdc943c8e378a5528b3d0bbe6a
Daisuke Fujita <dtanshi45@gmail.com>
2017-05-10 12:21:39 +0900

    Print running Pods
    copied from k8stail/main.go

--- main.go
+++ main.go
@@ -2,8 +2,93 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	flag "github.com/spf13/pflag"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/tools/clientcmd"
 )
 
 func main() {
-	fmt.Println("yeah!")
+	var (
+		kubeContext string
+		kubeconfig  string
+		labels      string
+		namespace   string
+	)
+
+	flags := flag.NewFlagSet("k8s-pod-notifier", flag.ExitOnError)
+	flags.Usage = func() {
+		flags.PrintDefaults()
+	}
+
+	flags.StringVar(&kubeContext, "context", "", "Kubernetes context")
+	flags.StringVar(&kubeconfig, "kubeconfig", "", "Path of kubeconfig")
+	flags.StringVarP(&labels, "labels", "l", "", "Label filter query")
+	flags.StringVarP(&namespace, "namespace", "n", "", "Kubernetes namespace")
+
+	if err := flags.Parse(os.Args[1:]); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	if kubeconfig == "" {
+		if os.Getenv("KUBECONFIG") != "" {
+			kubeconfig = os.Getenv("KUBECONFIG")
+		} else {
+			kubeconfig = clientcmd.RecommendedHomeFile
+		}
+	}
+
+	clientConfig := clientcmd.NewNonInteractiveDeferredLoadingClientConfig(
+		&clientcmd.ClientConfigLoadingRules{ExplicitPath: kubeconfig},
+		&clientcmd.ConfigOverrides{CurrentContext: kubeContext})
+
+	config, err := clientConfig.ClientConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	rawConfig, err := clientConfig.RawConfig()
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	var currentContext string
+
+	if kubeContext == "" {
+		currentContext = rawConfig.CurrentContext
+	} else {
+		currentContext = kubeContext
+	}
+
+	if namespace == "" {
+		if rawConfig.Contexts[currentContext].Namespace == "" {
+			namespace = v1.NamespaceDefault
+		} else {
+			namespace = rawConfig.Contexts[currentContext].Namespace
+		}
+	}
+
+	pods, err := clientset.Core().Pods(namespace).List(v1.ListOptions{})
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+
+	fmt.Println("context:   " + kubeContext)
+	fmt.Println("namespace: " + namespace)
+
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name)
+	}
 }
