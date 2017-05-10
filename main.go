81394c55bfcf84a9cdcdb8852e7fccf899251369
Daisuke Fujita <dtanshi45@gmail.com>
2017-05-10 14:18:44 +0900

    Watch Pod events

--- main.go
+++ main.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/signal"
 
 	flag "github.com/spf13/pflag"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/pkg/api/v1"
+	"k8s.io/client-go/pkg/watch"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -79,16 +82,59 @@ func main() {
 		}
 	}
 
-	pods, err := clientset.Core().Pods(namespace).List(v1.ListOptions{})
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, os.Interrupt)
+
+	watcher, err := clientset.Core().Pods(namespace).Watch(v1.ListOptions{
+		LabelSelector: labels,
+	})
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
 
-	fmt.Println("context:   " + kubeContext)
-	fmt.Println("namespace: " + namespace)
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
-	for _, pod := range pods.Items {
-		fmt.Println(pod.Name)
-	}
+	go func() {
+		for {
+			select {
+			case e := <-watcher.ResultChan():
+				if e.Object == nil {
+					return
+				}
+
+				pod, ok := e.Object.(*v1.Pod)
+				if !ok {
+					continue
+				}
+
+				switch e.Type {
+				case watch.Added:
+					if pod.Status.Phase != v1.PodRunning {
+						continue
+					}
+
+					fmt.Println("Added: " + pod.Name)
+
+				case watch.Modified:
+					switch pod.Status.Phase {
+					case v1.PodRunning:
+						fmt.Println("Added: " + pod.Name)
+					case v1.PodSucceeded:
+						fmt.Println("Succeeded: " + pod.Name)
+					case v1.PodFailed:
+						fmt.Println("Failed: " + pod.Name)
+					}
+				case watch.Deleted:
+					fmt.Println("Deleted: " + pod.Name)
+				}
+			case <-ctx.Done():
+				watcher.Stop()
+				return
+			}
+		}
+	}()
+
+	<-sigCh
 }
